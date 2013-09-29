26c8b64e036761eaebd0e5786e8bb487bd68402e
Nick Nguyen <nickhxnguyen@gmail.com>
2013-09-29 16:22:29 -0500

    added export code

--- lib/bellyc.rb
+++ lib/bellyc.rb
@@ -2,6 +2,7 @@ require 'bellyc/version.rb'
 require 'bellyc/crawler'
 require 'bellyc/checkin'
 require 'bellyc/parser'
+require 'ruby-progressbar'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
